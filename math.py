57abf893ff2a3a0826651cb5b54fddf1bfab62fc
vinaykumarnl <98751205+vinaykumarnl@users.noreply.github.com>
2022-01-31 19:44:12 +0530

    initial commit

---
+++ math.py
@@ -0,0 +1,14 @@
+# add implementation
+def add(a,b):
+    pass
+# subtraction 
+def sub(a,b):
+    pass
+
+# multiply implementation
+def mul(a,b):
+    pass
+
+# divide implementation
+def div(a,b):
+    pass
